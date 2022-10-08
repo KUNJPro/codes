8575328dc1bf47892bea2fd21b53c2729c7aec72
KUNJ <110556028+KUNJPro@users.noreply.github.com>
2022-10-08 16:56:17 +0530

    Create javaterm.java

---
+++ javaterm.java
@@ -0,0 +1,13 @@
+public class BinaryOppUse {
+    public static void main(String args[]) {
+
+        // (1) Binary AND Operator '&'
+        System.out.println((5 & 6));
+        System.out.println((10 & 7));
+
+        // (2) Binary OR Operators '|'
+        System.out.println((5 | 6));
+        System.out.println((10 | 7));
+      
+    }
+}
